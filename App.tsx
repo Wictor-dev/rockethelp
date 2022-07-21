cf6852d217f775bb50d3eb484b62806d186006d6
Wictor-dev <wictorgabrielmourao@gmail.com>
2022-07-21 09:08:35 -0300

    feat: terminado aula 2

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import { NativeBaseProvider, StatusBar } from 'native-base';
+import { NativeBaseProvider, StatusBar, View } from 'native-base';
 import React from 'react';
 import { SignIn } from './src/screens/SignIn';
 import { THEME } from './src/styles/theme';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/loading';
+import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,7 +17,7 @@ export default function App() {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle='light-content' backgroundColor="transparent" translucent />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <AppRoutes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
